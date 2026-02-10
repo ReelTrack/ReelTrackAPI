bc79237a0210a8c8f8b1ff7aeece78180cb6c393
VAC2UM <nikita.golowanev@gmail.com>
2026-02-10 18:31:59 +0300

    EDIT: добавлена работа с БД, пока без своей БД

---
+++ src/main/kotlin/Serialization.kt
@@ -0,0 +1,27 @@
+package com.example
+
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        gson {
+            }
+    }
+    routing {
+        get("/json/gson") {
+                call.respond(mapOf("hello" to "world"))
+            }
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
